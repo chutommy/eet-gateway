1ef2c4c1ed9fc0f768462a945e6ed6ef237c432f
chutommy <tommychu2256@gmail.com>
2021-09-02 20:02:42 +0200

    feat: Add Go src code autogen from WSDL files

--- gen.go
+++ gen.go
@@ -1,3 +1,4 @@
 package main
 
 //go:generate go run bin/soap-defs/main.go
+//go:generate wsdlgen -o pkg/definition-gen.go -pkg eet eet-specs/soap-definition/EETXMLSchema.xsd eet-specs/soap-definition/EETServiceSOAP.wsdl
