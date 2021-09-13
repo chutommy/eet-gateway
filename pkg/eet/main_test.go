78cbf8353ce58a9e7c4c647d9beec58316917306
Tommy Chu <tommychu2256@gmail.com>
2021-09-13 19:26:13 +0200

    refactor: Rename eet package files

---
+++ pkg/eet/main_test.go
@@ -0,0 +1,14 @@
+package eet_test
+
+import (
+	"io/ioutil"
+
+	"github.com/stretchr/testify/require"
+)
+
+func readFile(t require.TestingT, path string) []byte {
+	raw, err := ioutil.ReadFile(path)
+	require.NoError(t, err, "read file")
+
+	return raw
+}
