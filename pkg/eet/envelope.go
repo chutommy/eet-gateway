90af1d1e9009fc79afeb08090cdc54736d094717
Tommy Chu <tommychu2256@gmail.com>
2021-09-19 12:30:02 +0200

    refactor: Split SOAP envelope builder

--- pkg/eet/envelope.go
+++ pkg/eet/envelope.go
@@ -38,20 +38,21 @@ func NewSoapEnvelope(trzba *TrzbaType, ks dsig.X509KeyStore, crt *x509.Certifica
 		return nil, fmt.Errorf("set signature method: %w", err)
 	}
 
-	env := getEnvelope()
-	env.FindElement("./Envelope/Header/Security/BinarySecurityToken").SetText(string(binCrt))
-	env.FindElement("./Envelope/Body").AddChild(trzbaElem)
-	elemToSign := env.FindElement("./Envelope/Body")
-	elemToSign.CreateAttr("xmlns:u", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd")
-	elemToSign.CreateAttr("xmlns:s", "http://schemas.xmlsoap.org/soap/envelope/")
-	signature, err := signingContext.ConstructSignature(elemToSign, false)
+	bodyElem := buildBodyElem()
+	bodyElem.AddChild(trzbaElem)
+
+	signature, err := signingContext.ConstructSignature(bodyElem, false)
 	if err != nil {
 		return nil, fmt.Errorf("construct a signature: %w", err)
 	}
-	*(env.FindElement("./Envelope/Body")) = *elemToSign
+
+	env := getEnvelope()
+	env.FindElement("./Envelope/Header/Security/BinarySecurityToken").SetText(string(binCrt))
+	env.FindElement("./Envelope").AddChild(bodyElem)
+	*(env.FindElement("./Envelope/Body")) = *bodyElem
 	signatureElem := env.FindElement("./Envelope/Header/Security/Signature")
-	signatureElem.AddChild(signature.FindElement("./SignedInfo"))
-	signatureElem.AddChild(signature.FindElement("./SignatureValue"))
+	signatureElem.FindElement("./SignedInfo/Reference/DigestValue").SetText(signature.FindElement("./SignedInfo/Reference/DigestValue").Text())
+	signatureElem.FindElement("./SignatureValue").SetText(signature.FindElement("./SignatureValue").Text())
 
 	signedEnv, err := env.WriteToBytes()
 	if err != nil {
@@ -65,51 +66,90 @@ var envelopeTmpl *etree.Document
 
 func getEnvelope() *etree.Document {
 	if envelopeTmpl == nil {
-		envelopeTmpl = buildEnvelope()
+		envelopeTmpl = buildSOAPEnvelope()
 	}
 
 	return envelopeTmpl.Copy()
 }
 
-func buildEnvelope() *etree.Document {
+func buildSOAPEnvelope() *etree.Document {
 	doc := etree.NewDocument()
 	doc.CreateProcInst("xml", `version="1.0" encoding="UTF-8"`)
+	doc.SetRoot(buildEnvelope())
 
-	envelope := doc.CreateElement("Envelope")
+	return doc
+}
+
+func buildEnvelope() *etree.Element {
+	envelope := etree.NewElement("Envelope")
 	envelope.Space = "s"
 	envelope.CreateAttr("xmlns:u", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd")
 	envelope.CreateAttr("xmlns:s", "http://schemas.xmlsoap.org/soap/envelope/")
+	envelope.AddChild(buildHeader())
 
-	header := envelope.CreateElement("Header")
+	return envelope
+}
+
+func buildHeader() *etree.Element {
+	header := etree.NewElement("Header")
 	header.Space = "s"
+	header.AddChild(buildSecurity())
 
-	security := header.CreateElement("Security")
+	return header
+}
+
+func buildSecurity() *etree.Element {
+	security := etree.NewElement("Security")
 	security.Space = "wsse"
 	security.CreateAttr("xmlns:wsse", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd")
+	security.AddChild(buildBinarySecurityToken())
+	security.AddChild(buildSignature())
 
-	binarySecurityToken := security.CreateElement("BinarySecurityToken")
+	return security
+}
+
+func buildBinarySecurityToken() *etree.Element {
+	binarySecurityToken := etree.NewElement("BinarySecurityToken")
 	binarySecurityToken.Space = "wse"
 	binarySecurityToken.CreateAttr("xmlns:wse", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd")
 	binarySecurityToken.CreateAttr("EncodingType", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary")
 	binarySecurityToken.CreateAttr("ValueType", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3")
 	binarySecurityToken.CreateAttr("u:Id", "BinaryToken1")
 
-	signature := security.CreateElement("Signature")
+	return binarySecurityToken
+}
+
+func buildSignature() *etree.Element {
+	signature := etree.NewElement("Signature")
 	signature.CreateAttr("xmlns", "http://www.w3.org/2000/09/xmldsig#")
+	signature.AddChild(buildSignedInfo())
+	signature.CreateElement("SignatureValue")
+	signature.AddChild(buildKeyInfo())
 
-	// signedInfo := signature.CreateElement("SignedInfo")
-	// signedInfo.CreateElement("CanonicalizationMethod").CreateAttr("Algorithm", "http://www.w3.org/2001/10/xml-exc-c14n#")
-	// signedInfo.CreateElement("SignatureMethod").CreateAttr("Algorithm", "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256")
+	return signature
+}
 
-	// reference := signedInfo.CreateElement("Reference")
-	// reference.CreateAttr("URI", "#_1")
-	// reference.CreateElement("Transforms").CreateElement("Transform").CreateAttr("Algorithm", "http://www.w3.org/2001/10/xml-exc-c14n#")
-	// reference.CreateElement("DigestMethod").CreateAttr("Algorithm", "http://www.w3.org/2001/04/xmlenc#sha256")
-	// reference.CreateElement("DigestValue")
+func buildSignedInfo() *etree.Element {
+	signedInfo := etree.NewElement("SignedInfo")
+	signedInfo.CreateElement("CanonicalizationMethod").CreateAttr("Algorithm", "http://www.w3.org/2001/10/xml-exc-c14n#")
+	signedInfo.CreateElement("SignatureMethod").CreateAttr("Algorithm", "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256")
+	signedInfo.AddChild(buildReference())
 
-	// signature.CreateElement("SignatureValue")
+	return signedInfo
+}
 
-	keyInfo := signature.CreateElement("KeyInfo")
+func buildReference() *etree.Element {
+	reference := etree.NewElement("Reference")
+	reference.CreateAttr("URI", "#_1")
+	reference.CreateElement("Transforms").CreateElement("Transform").CreateAttr("Algorithm", "http://www.w3.org/2001/10/xml-exc-c14n#")
+	reference.CreateElement("DigestMethod").CreateAttr("Algorithm", "http://www.w3.org/2001/04/xmlenc#sha256")
+	reference.CreateElement("DigestValue")
+
+	return reference
+}
+
+func buildKeyInfo() *etree.Element {
+	keyInfo := etree.NewElement("KeyInfo")
 	securityTokenReference := keyInfo.CreateElement("SecurityTokenReference")
 	securityTokenReference.Space = "wse"
 	securityTokenReference.CreateAttr("xmlns:wse", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd")
@@ -118,9 +158,15 @@ func buildEnvelope() *etree.Document {
 	tokenReference.CreateAttr("URI", "#BinaryToken1")
 	tokenReference.CreateAttr("ValueType", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509")
 
-	body := envelope.CreateElement("Body")
-	body.Space = "s"
-	body.CreateAttr("u:Id", "_1")
+	return keyInfo
+}
 
-	return doc
+func buildBodyElem() *etree.Element {
+	body := etree.NewElement("Body")
+	body.CreateAttr("xmlns:u", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd")
+	body.CreateAttr("xmlns:s", "http://schemas.xmlsoap.org/soap/envelope/")
+	body.CreateAttr("u:Id", "_1")
+	body.Space = "s"
+
+	return body
 }
