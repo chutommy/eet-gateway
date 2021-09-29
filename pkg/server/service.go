bc6c42049e02c490e3079b05d5e697a24245e2c0
Tommy Chu <tommychu2256@gmail.com>
2021-09-29 19:21:45 +0200

    feat: Implement HTTP server service
    Signed-off-by: Tommy Chu <tommychu2256@gmail.com>

---
+++ pkg/server/service.go
@@ -0,0 +1,36 @@
+package server
+
+import (
+	"net/http"
+	"time"
+)
+
+// Service is the server abstraction as a service.
+type Service interface {
+	ListenAndServe() error
+}
+
+type httpService struct {
+	server *http.Server
+}
+
+// ListenAndServe runs the server and handles incoming requests.
+func (s *httpService) ListenAndServe() error {
+	return s.server.ListenAndServe()
+}
+
+// NewService returns a Service implementation.
+func NewService(handler Handler, addr string) Service {
+	return &httpService{
+		server: &http.Server{
+			Addr:    addr,
+			Handler: handler.httpHandler(),
+			// TLSConfig:         nil,
+			ReadTimeout:       time.Second * 10,
+			ReadHeaderTimeout: time.Second * 2,
+			WriteTimeout:      time.Second * 10,
+			IdleTimeout:       time.Second * 100,
+			MaxHeaderBytes:    http.DefaultMaxHeaderBytes,
+		},
+	}
+}
