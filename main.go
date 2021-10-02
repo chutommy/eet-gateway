7e1ffd884edb1f6ac135a11680f0626e30413f62
Tommy Chu <tommychu2256@gmail.com>
2021-10-02 09:41:25 +0200

    chore: Update snippet file
    Signed-off-by: Tommy Chu <tommychu2256@gmail.com>

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"encoding/pem"
 	"fmt"
 	"io/ioutil"
-	"time"
 
 	"github.com/chutommy/eetgateway/pkg/eet"
 	"github.com/chutommy/eetgateway/pkg/keystore"
@@ -18,66 +17,38 @@ import (
 )
 
 func main() {
-	gSvc := gatewaySvc()
-	h := server.NewHandler(gSvc)
-	srv := server.NewService(h, ":8080")
-	fmt.Println(srv.ListenAndServe())
-}
-
-func gatewaySvc() eet.GatewayService {
-	pbPK, pbCert := crypto()
-	pk, err := x509.ParsePKCS8PrivateKey(pbPK.Bytes) // ADAPTER
+	// pk and cert
+	rawCrt, err := ioutil.ReadFile("data/testdata/EET_CA1_Playground-CZ683555118.crt")
 	errCheck(err)
-	crt, err := wsse.ParseCertificate(pbCert)
+	pbCrt, _ := pem.Decode(rawCrt)
+	rawKey, err := ioutil.ReadFile("data/testdata/EET_CA1_Playground-CZ683555118.key")
+	errCheck(err)
+	pbPK, _ := pem.Decode(rawKey)
+	pk, err := x509.ParsePKCS8PrivateKey(pbPK.Bytes)
+	errCheck(err)
+	crt, err := wsse.ParseCertificate(pbCrt)
 	errCheck(err)
 
+	// dep services
 	c := mfcr.NewClient(mfcr.PlaygroundURL)
 	errCheck(err)
-
 	caSvc := mfcr.NewCAService()
-
 	ks := &ks{
 		key: pk.(*rsa.PrivateKey),
 		crt: crt,
 	}
-
 	gSvc := eet.NewGatewayService(c, caSvc, ks)
 
-	return gSvc
-}
-
-func crypto() (*pem.Block, *pem.Block) {
-	rawCrt, err := ioutil.ReadFile("data/testdata/EET_CA1_Playground-CZ683555118.crt")
-	errCheck(err)
-	crt, _ := pem.Decode(rawCrt)
-	rawKey, err := ioutil.ReadFile("data/testdata/EET_CA1_Playground-CZ683555118.key")
-	errCheck(err)
-	pk, _ := pem.Decode(rawKey)
-	return pk, crt
+	// server
+	h := server.NewHandler(gSvc)
+	srv := server.NewService(h, ":8080")
+	fmt.Println(srv.ListenAndServe())
 }
 
 func errCheck(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func mustParseTime(s string) time.Time {
-	t, err := parseTime(s)
-	if err != nil {
-		panic(err)
-	}
-
-	return t
-}
-
-func parseTime(s string) (time.Time, error) {
-	t, err := time.Parse(eet.DateTimeLayout, s)
-	if err != nil {
-		return t, fmt.Errorf("invalid time format: %w", err)
-	}
-
-	return t, nil
 }
 
 type ks struct {
