902c50b4cf3d5216532f6ddb2baf33e21a42b7f2
Tommy Chu <tommychu2256@gmail.com>
2021-10-15 21:42:18 +0200

    feature: Implement a symmetric encryption/decryption interface
    Signed-off-by: Tommy Chu <tommychu2256@gmail.com>

---
+++ pkg/keystore/keypair.go
@@ -0,0 +1,141 @@
+package keystore
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"io"
+)
+
+// ErrInvalidNonceSize is returned if then nonce is longer than the encrypted text.
+var ErrInvalidNonceSize = errors.New("invalid nonce size")
+
+// KeyPair represents a key pair combination of a private and public key.
+type KeyPair struct {
+	Cert *x509.Certificate
+	Key  *rsa.PrivateKey
+}
+
+func (kp *KeyPair) encrypt(password, salt []byte) (crt []byte, key []byte, err error) {
+	gcm, err := gcmCipher(salt, password)
+	if err != nil {
+		return nil, nil, fmt.Errorf("generate GCM: %w", err)
+	}
+
+	// encrypt public key
+	derPubKey := kp.Cert.Raw
+	crt, err = encryptPEMWithGCM(gcm, "CERTIFICATE", derPubKey)
+	if err != nil {
+		return nil, nil, fmt.Errorf("encrypt public key with GCM: %w", err)
+	}
+
+	// encrypt private key
+	derPrivKey := x509.MarshalPKCS1PrivateKey(kp.Key)
+	key, err = encryptPEMWithGCM(gcm, "RSA PRIVATE KEY", derPrivKey)
+	if err != nil {
+		return nil, nil, fmt.Errorf("encrypt private key with GCM: %w", err)
+	}
+
+	return crt, key, nil
+}
+
+func encryptPEMWithGCM(gcm cipher.AEAD, pemType string, data []byte) ([]byte, error) {
+	pemData := pem.EncodeToMemory(&pem.Block{
+		Type:  pemType,
+		Bytes: data,
+	})
+
+	// random nonce
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, fmt.Errorf("generate a random nonce: %w", err)
+	}
+
+	return gcm.Seal(nonce, nonce, pemData, nil), nil
+}
+
+func (kp *KeyPair) decrypt(password, salt, crt, key []byte) error {
+	gcm, err := gcmCipher(salt, password)
+	if err != nil {
+		return fmt.Errorf("generate GCM cipher: %w", err)
+	}
+
+	// certificate
+	crtPem, err := decryptPemWithGCM(gcm, crt)
+	if err != nil {
+		return fmt.Errorf("decrypt public key: %w", err)
+	}
+
+	kp.Cert, err = x509.ParseCertificate(crtPem)
+	if err != nil {
+		return fmt.Errorf("parse public key: %w", err)
+	}
+
+	// private key
+	pkPem, err := decryptPemWithGCM(gcm, key)
+	if err != nil {
+		return fmt.Errorf("decrypt private key: %w", err)
+	}
+
+	kp.Key, err = x509.ParsePKCS1PrivateKey(pkPem)
+	if err != nil {
+		return fmt.Errorf("parse private key: %w", err)
+	}
+
+	return nil
+}
+
+func decryptPemWithGCM(gcm cipher.AEAD, cipherText []byte) ([]byte, error) {
+	nonceSize := gcm.NonceSize()
+	// nonce is part of the cipher text and therefore must be smaller
+	if len(cipherText) < nonceSize {
+		return nil, fmt.Errorf("nonce is longer than cipher text: %w", ErrInvalidNonceSize)
+	}
+
+	// split nonce and the cipher
+	nonce, sealed := cipherText[:nonceSize], cipherText[nonceSize:]
+	// open sealed cipher
+	pemData, err := gcm.Open(nil, nonce, sealed, nil)
+	if err != nil {
+		return nil, fmt.Errorf("open sealed cipher text: %w", err)
+	}
+
+	block, _ := pem.Decode(pemData)
+
+	return block.Bytes, nil
+}
+
+func gcmCipher(salt, password []byte) (cipher.AEAD, error) {
+	salted := addSalt(salt, password)
+	hash := sha256.Sum256(salted)
+
+	c, err := aes.NewCipher(hash[:])
+	if err != nil {
+		return nil, fmt.Errorf("create a new cipher block: %w", err)
+	}
+
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		return nil, fmt.Errorf("create a GCM cipher mode: %w", err)
+	}
+
+	return gcm, nil
+}
+
+func addSalt(salt []byte, password []byte) []byte {
+	lp := len(password)
+	ls := len(salt)
+
+	// concatenate password and salt
+	out := make([]byte, lp+ls)
+	copy(out[:lp], password)
+	copy(out[lp:], salt)
+
+	return out
+}
--- pkg/keystore/service.go
+++ pkg/keystore/service.go
@@ -1,16 +1,11 @@
 package keystore
 
 import (
-	"crypto/rsa"
-	"crypto/x509"
+	"crypto/rand"
 	"errors"
+	"fmt"
+	"io"
 )
-
-// KeyPair represents a key pair combination of a private and public key.
-type KeyPair struct {
-	Cert *x509.Certificate
-	Key  *rsa.PrivateKey
-}
 
 // Service represents a keystore abstraction for a KeyPair management.
 type Service interface {
@@ -20,113 +15,37 @@ type Service interface {
 }
 
 type redisService struct {
-	crt []byte
-	key []byte
+	crt  []byte
+	key  []byte
+	salt []byte
 }
 
-var salt = []byte("_eetgateway")
-
 func (r *redisService) Store(id string, password []byte, kp *KeyPair) error {
-	panic(errors.New("implement me"))
-	/*
-		derPrivKey := x509.MarshalPKCS1PrivateKey(kp.Key)
-		privPem := pem.EncodeToMemory(&pem.Block{
-			Type:  "RSA PRIVATE KEY",
-			Bytes: derPrivKey,
-		})
+	salt := make([]byte, 256)
+	if _, err := io.ReadFull(rand.Reader, salt); err != nil {
+		return fmt.Errorf("generate a random salt value: %w", err)
+	}
 
-		pubPem := pem.EncodeToMemory(&pem.Block{
-			Type:  "CERTIFICATE",
-			Bytes: kp.Cert.Raw,
-		})
+	crt, key, err := kp.encrypt(password, salt)
+	if err != nil {
+		return fmt.Errorf("encrypt a keypair: %w", err)
+	}
 
-		salted := make([]byte, len(password))
-		copy(salted, password)
-		salted = append(salted, salt...)
-		hash := sha256.Sum256(salted)
-		c, err := aes.NewCipher(hash[:])
-		if err != nil {
-			panic(err)
-		}
+	r.key = key
+	r.crt = crt
+	r.salt = salt
 
-		gcm, err := cipher.NewGCM(c)
-		if err != nil {
-			panic(err)
-		}
-
-		noncePriv := make([]byte, gcm.NonceSize())
-		if _, err = io.ReadFull(rand.Reader, noncePriv); err != nil {
-			panic(err)
-		}
-
-		noncePub := make([]byte, gcm.NonceSize())
-		if _, err = io.ReadFull(rand.Reader, noncePub); err != nil {
-			panic(err)
-		}
-
-		r.key = gcm.Seal(noncePriv, noncePriv, privPem, nil)
-		r.crt = gcm.Seal(noncePub, noncePub, pubPem, nil)
-
-		return nil
-	*/
+	return nil
 }
 
 func (r *redisService) Get(id string, password []byte) (*KeyPair, error) {
-	panic(errors.New("implement me"))
-	/*
-		salted := make([]byte, len(password))
-		copy(salted, password)
-		salted = append(salted, salt...)
-		hash := sha256.Sum256(salted)
-		c, err := aes.NewCipher(hash[:])
-		if err != nil {
-			panic(err)
-		}
+	kp := new(KeyPair)
+	err := kp.decrypt(password, r.salt, r.crt, r.key)
+	if err != nil {
+		return nil, fmt.Errorf("decrypt a keypair: %w", err)
+	}
 
-		gcm, err := cipher.NewGCM(c)
-		if err != nil {
-			panic(err)
-		}
-
-		nonceSize := gcm.NonceSize()
-		if len(r.key) < nonceSize {
-			panic(err)
-		}
-		if len(r.crt) < nonceSize {
-			panic(err)
-		}
-
-		noncePriv, priv := r.key[:nonceSize], r.key[nonceSize:]
-		noncePub, pub := r.crt[:nonceSize], r.crt[nonceSize:]
-
-		privKey, err := gcm.Open(nil, noncePriv, priv, nil)
-		if err != nil {
-			panic(err)
-		}
-
-		pubKey, err := gcm.Open(nil, noncePub, pub, nil)
-		if err != nil {
-			panic(err)
-		}
-
-		privPem, _ := pem.Decode(privKey)
-		pubPem, _ := pem.Decode(pubKey)
-
-		pk, err := x509.ParsePKCS1PrivateKey(privPem.Bytes)
-		if err != nil {
-			panic(err)
-		}
-
-		crt, err := x509.ParseCertificate(pubPem.Bytes)
-		if err != nil {
-			panic(err)
-		}
-
-		return &KeyPair{
-			Cert: crt,
-			Key:  pk,
-		}, nil
-	*/
+	return kp, nil
 }
 
 func (r *redisService) Delete(id string, password []byte) error {
