7e25f48aea4e6add894e47eefead8da2e2dfe8b4
Tommy Chu <tommychu2256@gmail.com>
2021-10-16 16:25:50 +0200

    feat: Implement delete function for the keystore service
    Signed-off-by: Tommy Chu <tommychu2256@gmail.com>

--- pkg/keystore/service.go
+++ pkg/keystore/service.go
@@ -10,11 +10,14 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
+// ErrRecordNotFound is returned if the searched record can't be found.
+var ErrRecordNotFound = errors.New("record not found")
+
 // Service represents a keystore abstraction for a KeyPair management.
 type Service interface {
 	Store(ctx context.Context, id string, password []byte, kp *KeyPair) error
 	Get(ctx context.Context, id string, password []byte) (*KeyPair, error)
-	Delete(ctx context.Context, id string, password []byte) error
+	Delete(ctx context.Context, id string) error
 }
 
 type redisService struct {
@@ -28,6 +31,7 @@ var (
 )
 
 func (r *redisService) Store(ctx context.Context, id string, password []byte, kp *KeyPair) error {
+	// generate random salt for each reacord
 	salt := make([]byte, 256)
 	if _, err := io.ReadFull(rand.Reader, salt); err != nil {
 		return fmt.Errorf("generate a random salt value: %w", err)
@@ -58,12 +62,16 @@ func (r *redisService) Get(ctx context.Context, id string, password []byte) (*Ke
 		return nil, fmt.Errorf("retrieve stored certificate from database: %w", err)
 	}
 
-	crt := []byte(m[certificateField])
-	key := []byte(m[privateKeyField])
-	salt := []byte(m[saltField])
+	// check fields exist
+	crt, ok1 := m[certificateField]
+	key, ok2 := m[privateKeyField]
+	salt, ok3 := m[saltField]
+	if !(ok1 && ok2 && ok3) {
+		return nil, fmt.Errorf("empty certificate field: %w", ErrRecordNotFound)
+	}
 
 	kp := new(KeyPair)
-	err = kp.decrypt(password, salt, crt, key)
+	err = kp.decrypt(password, []byte(salt), []byte(crt), []byte(key))
 	if err != nil {
 		return nil, fmt.Errorf("decrypt a keypair: %w", err)
 	}
@@ -71,8 +79,18 @@ func (r *redisService) Get(ctx context.Context, id string, password []byte) (*Ke
 	return kp, nil
 }
 
-func (r *redisService) Delete(ctx context.Context, id string, password []byte) error {
-	panic(errors.New("implement me"))
+func (r *redisService) Delete(ctx context.Context, id string) error {
+	i, err := r.rdb.Del(ctx, id).Result()
+	if err != nil {
+		return fmt.Errorf("delete record from database: %w", err)
+	}
+
+	// check number of deleted records
+	if i == 0 {
+		return fmt.Errorf("delete record with ID: %s: %w", id, ErrRecordNotFound)
+	}
+
+	return nil
 }
 
 func NewRedisService(rdb *redis.Client) Service {
