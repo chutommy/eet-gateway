14f1714d024d69ab66f17d9cf024825165ea3f02
Tommy Chu <tommychu2256@gmail.com>
2021-10-23 16:17:27 +0200

    refactor: Simplify unit tests in the ca package
    Signed-off-by: Tommy Chu <tommychu2256@gmail.com>

--- pkg/ca/roots_test.go
+++ pkg/ca/roots_test.go
@@ -1,46 +1,18 @@
 package ca_test
 
 import (
-	"io/ioutil"
 	"testing"
 
 	"github.com/chutommy/eetgateway/pkg/ca"
-	"github.com/chutommy/eetgateway/pkg/fscr"
 	"github.com/stretchr/testify/require"
 )
 
-func TestPlaygroundRoots(t *testing.T) {
-	tests := []struct {
-		file     string
-		password string
-	}{
-		{
-			file:     "testdata/EET_CA1_Playground-CZ00000019.p12",
-			password: "eet",
-		},
-		{
-			file:     "testdata/EET_CA1_Playground-CZ683555118.p12",
-			password: "eet",
-		},
-		{
-			file:     "testdata/EET_CA1_Playground-CZ1212121218.p12",
-			password: "eet",
-		},
-	}
+func TestRoots(t *testing.T) {
+	prodRoots, err := ca.ProductionRoots()
+	require.NoError(t, err)
+	require.NotEmpty(t, prodRoots)
 
-	for _, tc := range tests {
-		t.Run(tc.file, func(t *testing.T) {
-			raw, err := ioutil.ReadFile(tc.file)
-			require.NoError(t, err, "file exists")
-
-			roots, err := ca.PlaygroundRoots()
-			require.NoError(t, err, "should be able to retrieve system root certificates")
-
-			caSvc := fscr.NewCAService(roots, nil)
-			cert, pk, err := caSvc.ParseTaxpayerCertificate(raw, tc.password)
-			require.NotNilf(t, cert, "valid taxpayer's certificate")
-			require.NotNilf(t, pk, "valid taxpayer's private key")
-			require.NoError(t, err, "valid taxpayer's p12 file")
-		})
-	}
+	pgRoots, err := ca.PlaygroundRoots()
+	require.NoError(t, err)
+	require.NotEmpty(t, pgRoots)
 }
