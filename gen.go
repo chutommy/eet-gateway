310f77b20f14dec96acf78a887a5209037dc6d50
Tommy Chu <tommychu2256@gmail.com>
2021-10-26 21:47:58 +0200

    test: Add mock entities and its go generate command
    Signed-off-by: Tommy Chu <tommychu2256@gmail.com>

--- gen.go
+++ gen.go
@@ -3,15 +3,13 @@ package main
 //go:generate go run cmd/soap-defs/main.go -dir data/eet-specs/
 
 // https://github.com/droyo/go-xml
-// https://github.com/fatih/gomodifytags
 //go:generate wsdlgen -o pkg/eet/eet-gen.go -pkg eet data/eet-specs/soap-definition/EETXMLSchema.xsd data/eet-specs/soap-definition/EETServiceSOAP.wsdl
+// https://github.com/fatih/gomodifytags
 //go:generate gomodifytags -file pkg/eet/eet-gen.go -struct OdpovedType -remove-tags xml -w --quiet
 //go:generate gomodifytags -file pkg/eet/eet-gen.go -struct OdpovedType -add-tags xml -add-options xml=omitempty -transform pascalcase -w --quiet
 //go:generate gomodifytags -file pkg/eet/eet-gen.go -struct TrzbaType -remove-tags xml -w --quiet
 //go:generate gomodifytags -file pkg/eet/eet-gen.go -struct TrzbaType -add-tags xml -transform pascalcase -w --quiet
 //go:generate gomodifytags -file pkg/eet/eet-gen.go -struct TrzbaKontrolniKodyType -remove-tags xml -w --quiet
 //go:generate gomodifytags -file pkg/eet/eet-gen.go -struct TrzbaKontrolniKodyType -add-tags xml -transform snakecase -w --quiet
-
-// https://github.com/fatih/gomodifytags
-// disabled
-// go:generate gomodifytags -file pkg/eet/eet-gen.go -all -add-tags json -add-options json=omitempty -transform camelcase --skip-unexported -w --quiet
+// https://github.com/vektra/mockery
+//go:generate mockery --all --dir pkg --output pkg/mocks --case snake --note "EETGateway - Tommy Chu"
