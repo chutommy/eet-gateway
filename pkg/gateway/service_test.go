03d89b8f48467d0f6a24182ac87772e9f67958ce
Tommy Chu <tommychu2256@gmail.com>
2021-10-30 16:42:53 +0200

    test: Add unit tests for managing certificates in the gateway package
    Signed-off-by: Tommy Chu <tommychu2256@gmail.com>

--- pkg/gateway/service_test.go
+++ pkg/gateway/service_test.go
@@ -2,13 +2,61 @@ package gateway_test
 
 import (
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
 	"errors"
+	"math/big"
 	"testing"
+	"time"
 
+	"github.com/chutommy/eetgateway/pkg/fscr"
 	"github.com/chutommy/eetgateway/pkg/gateway"
+	"github.com/chutommy/eetgateway/pkg/keystore"
 	mfscr "github.com/chutommy/eetgateway/pkg/mocks/fscr"
 	mkeystore "github.com/chutommy/eetgateway/pkg/mocks/keystore"
 	"github.com/stretchr/testify/require"
+)
+
+var defaultCertTmpl = &x509.Certificate{
+	SerialNumber:          big.NewInt(1),
+	NotBefore:             time.Now(),
+	NotAfter:              time.Now().Add(time.Minute),
+	BasicConstraintsValid: true,
+	KeyUsage:              x509.KeyUsageDigitalSignature,
+	ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageAny},
+}
+
+func randomKeyPair() *keystore.KeyPair {
+	pk, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(err)
+	}
+
+	der, err := x509.CreateCertificate(rand.Reader, defaultCertTmpl, defaultCertTmpl, pk.Public(), pk)
+	if err != nil {
+		panic(err)
+	}
+
+	cert, err := x509.ParseCertificate(der)
+	if err != nil {
+		panic(err)
+	}
+
+	return &keystore.KeyPair{
+		Cert: cert,
+		PK:   pk,
+	}
+}
+
+var (
+	certID        = "cert1"
+	certID2       = "cert2"
+	certPassword  = []byte("secret1")
+	certPassword2 = []byte("secret2")
+	certKP        = randomKeyPair()
+	pkcsData      = []byte("p12 data")
+	pkcsPassword  = "secret2"
 )
 
 func TestService_Ping(t *testing.T) {
@@ -69,6 +117,356 @@ func TestService_Ping(t *testing.T) {
 			}
 
 			fscrClient.AssertExpectations(t)
+			fscrClient.AssertExpectations(t)
+			keystoreService.AssertExpectations(t)
+		})
+	}
+}
+
+func TestService_StoreCert(t *testing.T) {
+	tests := []struct {
+		name  string
+		setup func(ca *mfscr.CAService, ks *mkeystore.Service)
+		errs  []error
+	}{
+		{
+			name: "ok",
+			setup: func(cas *mfscr.CAService, ks *mkeystore.Service) {
+				cas.On("ParseTaxpayerCertificate", pkcsData, pkcsPassword).Return(certKP.Cert, certKP.PK, nil)
+				ks.On("Store", context.Background(), certID, certPassword, certKP).Return(nil)
+			},
+			errs: nil,
+		},
+		{
+			name: "invalid certificate",
+			setup: func(cas *mfscr.CAService, ks *mkeystore.Service) {
+				cas.On("ParseTaxpayerCertificate", pkcsData, pkcsPassword).Return(nil, nil, fscr.ErrInvalidCertificate)
+			},
+			errs: []error{
+				gateway.ErrInvalidTaxpayersCertificate,
+			},
+		},
+		{
+			name: "unknown certificate parse error",
+			setup: func(cas *mfscr.CAService, ks *mkeystore.Service) {
+				cas.On("ParseTaxpayerCertificate", pkcsData, pkcsPassword).Return(nil, nil, errors.New("parse error"))
+			},
+			errs: []error{
+				gateway.ErrCertificateParse,
+			},
+		},
+		{
+			name: "id already exists",
+			setup: func(cas *mfscr.CAService, ks *mkeystore.Service) {
+				cas.On("ParseTaxpayerCertificate", pkcsData, pkcsPassword).Return(certKP.Cert, certKP.PK, nil)
+				ks.On("Store", context.Background(), certID, certPassword, certKP).Return(keystore.ErrIDAlreadyExists)
+			},
+			errs: []error{
+				gateway.ErrIDAlreadyExists,
+			},
+		},
+		{
+			name: "max tries of db transactions",
+			setup: func(cas *mfscr.CAService, ks *mkeystore.Service) {
+				cas.On("ParseTaxpayerCertificate", pkcsData, pkcsPassword).Return(certKP.Cert, certKP.PK, nil)
+				ks.On("Store", context.Background(), certID, certPassword, certKP).Return(keystore.ErrReachedMaxRetries)
+			},
+			errs: []error{
+				gateway.ErrRequestDiscarded,
+			},
+		},
+		{
+			name: "unknown certificate store error",
+			setup: func(cas *mfscr.CAService, ks *mkeystore.Service) {
+				cas.On("ParseTaxpayerCertificate", pkcsData, pkcsPassword).Return(certKP.Cert, certKP.PK, nil)
+				ks.On("Store", context.Background(), certID, certPassword, certKP).Return(errors.New("store error"))
+			},
+			errs: []error{
+				gateway.ErrCertificateStore,
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			fscrClient := new(mfscr.Client)
+			caService := new(mfscr.CAService)
+			keystoreService := new(mkeystore.Service)
+
+			tc.setup(caService, keystoreService)
+
+			g := gateway.NewService(fscrClient, caService, keystoreService)
+			err := g.StoreCert(context.Background(), certID, certPassword, pkcsData, pkcsPassword)
+			if tc.errs == nil {
+				require.NoError(t, err)
+			} else {
+				for _, e := range tc.errs {
+					require.ErrorIs(t, err, e)
+				}
+			}
+
+			fscrClient.AssertExpectations(t)
+			caService.AssertExpectations(t)
+			keystoreService.AssertExpectations(t)
+		})
+	}
+}
+
+func TestService_ListCertIDs(t *testing.T) {
+	tests := []struct {
+		name  string
+		setup func(ks *mkeystore.Service)
+		errs  []error
+	}{
+		{
+			name: "ok",
+			setup: func(ks *mkeystore.Service) {
+				ks.On("List", context.Background()).Return([]string{certID}, nil)
+			},
+			errs: nil,
+		},
+		{
+			name: "unknown list certificates error",
+			setup: func(ks *mkeystore.Service) {
+				ks.On("List", context.Background()).Return(nil, errors.New("list error"))
+			},
+			errs: []error{
+				gateway.ErrListCertIDs,
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			fscrClient := new(mfscr.Client)
+			caService := new(mfscr.CAService)
+			keystoreService := new(mkeystore.Service)
+
+			tc.setup(keystoreService)
+
+			g := gateway.NewService(fscrClient, caService, keystoreService)
+			ids, err := g.ListCertIDs(context.Background())
+			if tc.errs == nil {
+				require.NoError(t, err)
+				require.NotEmpty(t, ids)
+			} else {
+				for _, e := range tc.errs {
+					require.ErrorIs(t, err, e)
+				}
+			}
+
+			fscrClient.AssertExpectations(t)
+			caService.AssertExpectations(t)
+			keystoreService.AssertExpectations(t)
+		})
+	}
+}
+
+func TestService_UpdateCertID(t *testing.T) {
+	tests := []struct {
+		name  string
+		setup func(ks *mkeystore.Service)
+		errs  []error
+	}{
+		{
+			name: "ok",
+			setup: func(ks *mkeystore.Service) {
+				ks.On("UpdateID", context.Background(), certID, certID2).Return(nil)
+			},
+			errs: nil,
+		},
+		{
+			name: "certificate not found",
+			setup: func(ks *mkeystore.Service) {
+				ks.On("UpdateID", context.Background(), certID, certID2).Return(keystore.ErrRecordNotFound)
+			},
+			errs: []error{
+				gateway.ErrCertificateNotFound,
+			},
+		},
+		{
+			name: "certificate id already exists",
+			setup: func(ks *mkeystore.Service) {
+				ks.On("UpdateID", context.Background(), certID, certID2).Return(keystore.ErrIDAlreadyExists)
+			},
+			errs: []error{
+				gateway.ErrIDAlreadyExists,
+			},
+		},
+		{
+			name: "max tries of db transactions",
+			setup: func(ks *mkeystore.Service) {
+				ks.On("UpdateID", context.Background(), certID, certID2).Return(keystore.ErrReachedMaxRetries)
+			},
+			errs: []error{
+				gateway.ErrRequestDiscarded,
+			},
+		},
+		{
+			name: "unknown update certificate id error",
+			setup: func(ks *mkeystore.Service) {
+				ks.On("UpdateID", context.Background(), certID, certID2).Return(errors.New("update certificate id error"))
+			},
+			errs: []error{
+				gateway.ErrCertificateUpdateID,
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			fscrClient := new(mfscr.Client)
+			caService := new(mfscr.CAService)
+			keystoreService := new(mkeystore.Service)
+
+			tc.setup(keystoreService)
+
+			g := gateway.NewService(fscrClient, caService, keystoreService)
+			err := g.UpdateCertID(context.Background(), certID, certID2)
+			if tc.errs == nil {
+				require.NoError(t, err)
+			} else {
+				for _, e := range tc.errs {
+					require.ErrorIs(t, err, e)
+				}
+			}
+
+			fscrClient.AssertExpectations(t)
+			caService.AssertExpectations(t)
+			keystoreService.AssertExpectations(t)
+		})
+	}
+}
+
+func TestService_UpdateCertPassword(t *testing.T) {
+	tests := []struct {
+		name  string
+		setup func(ks *mkeystore.Service)
+		errs  []error
+	}{
+		{
+			name: "ok",
+			setup: func(ks *mkeystore.Service) {
+				ks.On("UpdatePassword", context.Background(), certID, certPassword, certPassword2).Return(nil)
+			},
+			errs: nil,
+		},
+		{
+			name: "certificate not found",
+			setup: func(ks *mkeystore.Service) {
+				ks.On("UpdatePassword", context.Background(), certID, certPassword, certPassword2).Return(keystore.ErrRecordNotFound)
+			},
+			errs: []error{
+				gateway.ErrCertificateNotFound,
+			},
+		},
+		{
+			name: "invalid certificate password",
+			setup: func(ks *mkeystore.Service) {
+				ks.On("UpdatePassword", context.Background(), certID, certPassword, certPassword2).Return(keystore.ErrInvalidDecryptionKey)
+			},
+			errs: []error{
+				gateway.ErrInvalidCertificatePassword,
+			},
+		},
+		{
+			name: "max tries of db transactions",
+			setup: func(ks *mkeystore.Service) {
+				ks.On("UpdatePassword", context.Background(), certID, certPassword, certPassword2).Return(keystore.ErrReachedMaxRetries)
+			},
+			errs: []error{
+				gateway.ErrRequestDiscarded,
+			},
+		},
+		{
+			name: "unknown update certificate password error",
+			setup: func(ks *mkeystore.Service) {
+				ks.On("UpdatePassword", context.Background(), certID, certPassword, certPassword2).Return(errors.New("update certificate password error"))
+			},
+			errs: []error{
+				gateway.ErrCertificateUpdatePassword,
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			fscrClient := new(mfscr.Client)
+			caService := new(mfscr.CAService)
+			keystoreService := new(mkeystore.Service)
+
+			tc.setup(keystoreService)
+
+			g := gateway.NewService(fscrClient, caService, keystoreService)
+			err := g.UpdateCertPassword(context.Background(), certID, certPassword, certPassword2)
+			if tc.errs == nil {
+				require.NoError(t, err)
+			} else {
+				for _, e := range tc.errs {
+					require.ErrorIs(t, err, e)
+				}
+			}
+
+			fscrClient.AssertExpectations(t)
+			caService.AssertExpectations(t)
+			keystoreService.AssertExpectations(t)
+		})
+	}
+}
+
+func TestService_DeleteID(t *testing.T) {
+	tests := []struct {
+		name  string
+		setup func(ks *mkeystore.Service)
+		errs  []error
+	}{
+		{
+			name: "ok",
+			setup: func(ks *mkeystore.Service) {
+				ks.On("Delete", context.Background(), certID).Return(nil)
+			},
+			errs: nil,
+		},
+		{
+			name: "certificate not found",
+			setup: func(ks *mkeystore.Service) {
+				ks.On("Delete", context.Background(), certID).Return(keystore.ErrRecordNotFound)
+			},
+			errs: []error{
+				gateway.ErrCertificateNotFound,
+			},
+		},
+		{
+			name: "unknown certificate delete error",
+			setup: func(ks *mkeystore.Service) {
+				ks.On("Delete", context.Background(), certID).Return(errors.New("certificate delete error"))
+			},
+			errs: []error{
+				gateway.ErrCertificateDelete,
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			fscrClient := new(mfscr.Client)
+			caService := new(mfscr.CAService)
+			keystoreService := new(mkeystore.Service)
+
+			tc.setup(keystoreService)
+
+			g := gateway.NewService(fscrClient, caService, keystoreService)
+			err := g.DeleteID(context.Background(), certID)
+			if tc.errs == nil {
+				require.NoError(t, err)
+			} else {
+				for _, e := range tc.errs {
+					require.ErrorIs(t, err, e)
+				}
+			}
+
+			fscrClient.AssertExpectations(t)
+			caService.AssertExpectations(t)
 			keystoreService.AssertExpectations(t)
 		})
 	}
