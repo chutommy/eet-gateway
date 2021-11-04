f584b9170875340ebc0381c68f7c5d5e5dd671e5
Tommy Chu <tommychu2256@gmail.com>
2021-11-04 08:11:19 +0100

    feat: Implement version command

---
+++ pkg/cmd/versioncmd.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of the EET Gateway",
+	Long: `Version command prints out the current version of the EET
+Gateway and the OS/architecture of the binary build.`,
+	Args: cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		s := fmt.Sprintf("EET Gateway v%s %s/%s", eetgVersion, runtime.GOOS, runtime.GOARCH)
+		fmt.Println(s)
+	},
+}
